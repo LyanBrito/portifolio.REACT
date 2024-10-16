7f4dbf935a3d8f1d25c2ac26531628062d42edba
Lyan <lyanbrito613@gmail.com>
2024-10-15 21:57:33 -0300

    feat:added-square-elements

---
+++ src/components/pages/fixed-elements/square/2125.jsx
@@ -0,0 +1,5 @@
+import styles from "./square.module.css"
+
+export default function Square2125() {
+  return <div className={styles.square2125}></div>
+}
---
+++ src/components/pages/fixed-elements/square/672.jsx
@@ -0,0 +1,5 @@
+import styles from "./square.module.css"
+
+export default function Square672() {
+  return <div className={styles.square672}></div>
+}
