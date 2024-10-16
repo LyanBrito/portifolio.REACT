ae86e5b0f2cf63ed4c073974ee7bf7df29ea26b2
Lyan <lyanbrito613@gmail.com>
2024-10-15 21:55:38 -0300

    feat:added-about-me-and-fixed-elements

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ import HeaderPage from "./components/pages/header";
 import SocialMedia from "./components/pages/fixed-elements/social-media";
 import HeroPage from "./components/pages/main";
 import QuotePage from "./components/pages/quote";
-import Square from "./components/pages/fixed-elements/square";
+import Square672 from "./components/pages/fixed-elements/square/672";
 import Projects from "./components/pages/projects";
 import Skills from "./components/pages/skills";
+import AboutMe from "./components/pages/aboutMe";
+import Square1160 from "./components/pages/fixed-elements/square/1160";
+import Square2125 from "./components/pages/fixed-elements/square/2125";
+import Dot932 from "./components/pages/fixed-elements/dots/932";
+import Dot2294 from "./components/pages/fixed-elements/dots/2294";
 
 
 
@@ -17,9 +22,14 @@ export default function App() {
       <SocialMedia />
       <HeroPage />
       <QuotePage />
-      <Square />
+      <Square672 />
+      <Square1160 />
+      <Square2125 />
+      <Dot932 />
+      <Dot2294 />
       <Projects />
       <Skills />
+      <AboutMe />
     </div>
   );
 }
