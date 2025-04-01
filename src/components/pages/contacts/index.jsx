c25684af63fbc441e794e009a514158d80dbee81
LyanBrito <lyanbrito613@gmail.com>
2025-04-01 17:18:32 -0300

    feat: changed contact options

--- src/components/pages/contacts/index.jsx
+++ src/components/pages/contacts/index.jsx
@@ -1,5 +1,5 @@
 import styles from "./contacts.module.css";
-import DiscordIcon from "../../assets/svg/discord-icon"
+import GitHubLogo from "../../assets/svg/github-logo"
 import EmailIcon from "../../assets/svg/email-icon"
 import Dot2705 from "../fixed-elements/dots/2705";
 
@@ -19,13 +19,13 @@ export default function Contacts() {
         </p>
         <div className={styles.contacts}>
           <h6>Message me here</h6>
-          <a href="#" className={styles.contactOption}>
-            <a href="#">{DiscordIcon}</a>
-            !Elias#3519
+          <a href="https://github.com/LyanBrito" className={styles.contactOption}>
+            <a href="https://github.com/LyanBrito">{GitHubLogo}</a>
+            /lyanbrito
           </a>
           <a href="#" className={styles.contactOption}>
             <a href="mailto:lyanbrito613@gmail.com">{EmailIcon}</a>
-            elias@elias.me
+            lyanbrito613@gmail.com
           </a>
         </div>
       </div>
