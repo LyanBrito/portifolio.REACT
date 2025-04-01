42090e473afaf527d6092920b4feaba24b053372
LyanBrito <lyanbrito613@gmail.com>
2025-04-01 17:25:50 -0300

    feat: changed about me

--- src/components/pages/aboutMe/index.jsx
+++ src/components/pages/aboutMe/index.jsx
@@ -16,16 +16,13 @@ export default function AboutMe() {
             Hello, i’m Elias! 
             <br />
             <br />
-             I’m a self-taught front-end developer based in
-            Kyiv, Ukraine. I can develop responsive websites from scratch and
-            raise them into modern user-friendly web experiences. 
-            <br />
-            <br />
-             Transforming
-            my creativity and knowledge into a websites has been my passion for
-            over a year. I have been helping various clients to establish their
-            presence online. I always strive to learn about the newest
-            technologies and frameworks.
+            I'm a developer passionate about technology and always looking for new challenges. 
+            Throughout my journey, I gained experience in several areas, from front-end development to
+            modern frameworks and agile methodologies. Based on personal study and free courses at SENAI.
+            
+            I started a technical systems development course at SENAI Suíço-Brasileiro, where I am further 
+            enhancing my skills and knowledge. I am constantly looking to improve and expand my expertise 
+            in the world of technology.
           </p>
           <button>{"Read More ->"}</button>
         </div> 
