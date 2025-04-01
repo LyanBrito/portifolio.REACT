f24b8ec8aeb68a8b64c775d254ddb1705eee0803
LyanBrito <lyanbrito613@gmail.com>
2025-04-01 17:22:28 -0300

    feat: changed skills

--- src/components/pages/skills/index.jsx
+++ src/components/pages/skills/index.jsx
@@ -15,23 +15,23 @@ export default function Skills() {
         <div className={styles.skills}>
           <div className={styles.skill}>
             <h6>Languages</h6>
-            <p>TypeScript Lua Python JavaScript</p>
+            <p>TypeScript Java Python JavaScript</p>
           </div>
           <div className={styles.skill}>
             <h6>Databases</h6>
-            <p>SQLite PostgreSQL Mongo</p>
+            <p>MySQL PostgreSQL Mongo</p>
           </div>
           <div className={styles.skill}>
-            <h6>Other</h6>
-            <p>HTML CSS EJS SCSS REST Jinja</p>
+            <h6>Others</h6>
+            <p>HTML SCSS REST</p>
           </div>
           <div className={styles.skill}>
             <h6>Tools</h6>
-            <p>VSCode Neovim Linux Figma XFCE Arch Git Font Awesome</p>
+            <p>VSCode IntelliJ Figma Notion Git WebStorm</p>
           </div>
           <div className={styles.skill}>
             <h6>Frameworks</h6>
-            <p>React Vue Disnake Discord.js Flask Express.js</p>
+            <p>React Tailwind Less Astro Express.js</p>
           </div>
         </div>
       </div>
